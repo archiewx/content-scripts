9f9112a17f507844f140922fa5d76d0ae7121250
zsirfs <zhenglfsir@gmail.com>
2020-05-26 09:59:26 +0800

    feat: 原地替换

---
+++ place-in-place-remove-duplicate.js
@@ -0,0 +1,15 @@
+// 已排序原地去重
+
+function removeDuplicate(nums = []) {
+  let counter = 0;
+  for (let index = 1; index < nums.length; index++) {
+    if (nums[counter] !== nums[index]) {
+      nums[++counter] = nums[index]
+    }
+  }
+  return counter + 1
+}
+
+const nums = [0, 0, 1, 1, 1, 3, 3, 5, 6]
+console.log(removeDuplicate(nums))
+console.log(nums)
