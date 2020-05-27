88933a8ed255077510bda7f0992122f62da7b98c
zsirfs <zhenglfsir@gmail.com>
2020-05-27 23:42:15 +0800

    feat: 简单eventemitter实现,on, off, emit, once

---
+++ event-emitter.js
@@ -0,0 +1,61 @@
+class EventEmitter {
+  constructor() {
+    this.queue = [];
+  }
+
+  on(key, fn) {
+    const item = this.queue.find((item) => item.key === key);
+
+    if (!item) {
+      this.queue.push({ key, listeners: [fn] });
+    } else {
+      item.listeners.push(fn);
+    }
+  }
+
+  off(key) {
+    this.queue = this.queue.filter((item) => item.key === key);
+  }
+
+  once(key, fn) {
+    const item = this.queue.find((item) => item.key === key);
+    if (!item) this.queue.push({ key, tempListeners: [fn] });
+    else item.tempListeners.push(fn);
+  }
+
+  emit(key, ...args) {
+    this.queue
+      .filter((item) => item.key === key)
+      .forEach((item) => {
+        if (item.listeners) item.listeners.forEach((listener) => listener(...args));
+
+        while (item.tempListeners && item.tempListeners.length) {
+          const tmpListener = item.tempListeners.pop();
+          tmpListener(...args);
+        }
+      });
+  }
+}
+
+const emitter = new EventEmitter();
+
+emitter.on('hello', (...args) => {
+  console.log('1-->', ...args);
+});
+emitter.on('hello', (...args) => {
+  console.log('2-->', ...args);
+});
+emitter.once('say', (...args) => {
+  console.log('3-->', ...args);
+});
+
+emitter.emit('hello', 'zs');
+
+setTimeout(() => {
+  emitter.emit('say', 'hello world');
+  emitter.emit('say', 'hello world');
+  emitter.emit('say', 'hello world');
+emitter.emit('hello', 'zs');
+emitter.emit('hello', 'zs');
+emitter.emit('hello', 'zs');
+}, 2000);
