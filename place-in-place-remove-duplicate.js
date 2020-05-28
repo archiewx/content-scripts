94db2d74154df80a7634e83776d6ac7e23c598a5
zsirfs <zhenglfsir@gmail.com>
2020-05-28 11:40:14 +0800

    feat: 新增测试用例

--- place-in-place-remove-duplicate.js
+++ place-in-place-remove-duplicate.js
@@ -13,3 +13,7 @@ function removeDuplicate(nums = []) {
 const nums = [0, 0, 1, 1, 1, 3, 3, 5, 6]
 console.log(removeDuplicate(nums))
 console.log(nums)
+
+const array = [1,1, '1', 'NaN', NaN, {a:1}, {a:1}, '{a:1}']
+console.log(removeDuplicate(array))
+console.log(array)
