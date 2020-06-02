b82da31db9a178ab4aa2ac530a28efd64f9dddd9
zsirfs <zhenglfsir@gmail.com>
2020-06-02 19:06:42 +0800

    feat: 洗牌算法注释

--- shuffle.js
+++ shuffle.js
@@ -3,6 +3,7 @@
 function shuffle(array = []) {
   const cloneArray = array.slice();
 
+  // 依次对在 0 -> i 范围内的元素，随机指定一个位置，以达到洗牌的目的
   for (let index = cloneArray.length - 1; index > 0; index--) {
     const randomIndex = Math.floor(Math.random() * (index + 1));
     [cloneArray[index], cloneArray[randomIndex]] = [cloneArray[randomIndex], cloneArray[index]];
