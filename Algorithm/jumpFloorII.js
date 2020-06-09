0ec20919fc06886ebc0c4de7b4b682ce9eaabc5c
zsirfs <zhenglfsir@gmail.com>
2020-06-09 13:41:13 +0800

    feat: 青蛙跳台阶

--- Algorithm/jumpFloorII.js
+++ Algorithm/jumpFloorII.js
@@ -1,2 +1,10 @@
 function jumpFloorII(number) {
+  if (number === 0) return -1;
+
+  if (number === 1) return 1;
+
+  // 状态房产 f(number) = 2 * f(number - 1)
+  return 2 * jumpFloorII(number - 1);
 }
+
+console.log(jumpFloorII(20))
