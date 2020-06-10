d13b210e630a0aae4c13a25f6dfb2d2c846cd3a4
zsirfs <zhenglfsir@gmail.com>
2020-06-10 13:42:06 +0800

    feat: 计算综合

--- Algorithm/blow-candle.js
+++ Algorithm/blow-candle.js
@@ -1,5 +1,6 @@
 // 一个人总共吹了253根蜡烛，它几岁了【一岁吹一根，两岁吹两根，依次类推】
 
+// 动态规划
 function blowCandle(number) {
   let i = 1,
     sum = 0;
@@ -9,6 +10,19 @@ function blowCandle(number) {
   // 以为上面多加了1
   return i - 1;
 }
+
+// 递归解决
+function recursiveBlowCandle(number) {
+  let num = 0;
+  const run = function (number) {
+    if (number <= 0) return num;
+    return run(number - ++num);
+  };
+  run(number);
+  return num
+}
+
+console.log(recursiveBlowCandle(253));
 
 console.log(blowCandle(253));
 
